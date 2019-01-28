ca6fa8b6b25880f38ca61308c8a8768dff05bba1
Nick Nicholas <opoudjis@optusnet.com.au>
2019-01-28 01:02:37 +0000

    better handling of CR in XML; https://github.com/riboseinc/metanorma-standoc/issues/43

--- lib/asciidoctor/ogc/converter.rb
+++ lib/asciidoctor/ogc/converter.rb
@@ -27,7 +27,7 @@ module Asciidoctor
         result << noko { |ixml| front node, ixml }
         result << noko { |ixml| middle node, ixml }
         result << "</ogc-standard>"
-        result = textcleanup(result.flatten * "\n")
+        result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
         validate(ret1)
         ret1.root.add_namespace(nil, Metanorma::Ogc::DOCUMENT_NAMESPACE)
