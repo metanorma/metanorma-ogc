37219e8ed81e99ca7f54cce9d3aa61f31b6c2687
Nick Nicholas <opoudjis@optusnet.com.au>
2019-04-05 23:37:30 +1100

    Proper inheritance of example conversion; closes https://github.com/metanorma/mn-templates-ogc/issues/3

--- lib/asciidoctor/ogc/converter.rb
+++ lib/asciidoctor/ogc/converter.rb
@@ -174,6 +174,9 @@ module Asciidoctor
 
       def example(node)
         return term_example(node) if in_terms?
+        return requirement(node, "recommendation") if node.attr("style") == "recommendation"
+        return requirement(node, "requirement") if node.attr("style") == "requirement"
+        return requirement(node, "permission") if node.attr("style") == "permission"
         noko do |xml|
           xml.example **id_attr(node) do |ex|
             figure_title(node, ex)
