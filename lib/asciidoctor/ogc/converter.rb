db82d0bf6a0ad7ee1d4211af5d4ddd53b16662e4
Nick Nicholas <opoudjis@gmail.com>
2020-03-23 20:55:20 +1100

    file location of err file: https://github.com/metanorma/metanorma-standoc/issues/223

--- lib/asciidoctor/ogc/converter.rb
+++ lib/asciidoctor/ogc/converter.rb
@@ -62,7 +62,7 @@ module Asciidoctor
           word_converter(node).convert filename unless node.attr("nodoc")
           pdf_converter(node).convert filename unless node.attr("nodoc")
         end
-        @log.write(@filename + ".err") unless @novalid
+        @log.write(@localdir + @filename + ".err") unless @novalid
         @files_to_delete.each { |f| FileUtils.rm f }
         ret
       end
