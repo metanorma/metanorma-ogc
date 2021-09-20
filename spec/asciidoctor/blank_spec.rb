72203a14326d604a7bd5c4c8f4095a300f12fcc2
Nick Nicholas <opoudjis@gmail.com>
2021-09-20 22:12:12 +1000

    modspec synonyms: https://github.com/metanorma/metanorma-ogc/issues/276

---
+++ spec/asciidoctor/blank_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+require "fileutils"
+
+RSpec.describe Asciidoctor::Ogc do
+  it "processes a blank document" do
+    input = <<~"INPUT"
+      #{ASCIIDOC_BLANK_HDR}
+    INPUT
+
+    output = xmlpp(<<~"OUTPUT")
+          #{BLANK_HDR}
+          <preface>#{SECURITY}</preface>
+      <sections/>
+      </ogc-standard>
+    OUTPUT
+
+    expect(xmlpp(strip_guid(Asciidoctor.convert(input, *OPTIONS))))
+      .to be_equivalent_to output
+  end
+
+  it "converts a blank document" do
+    input = <<~"INPUT"
+      = Document title
+      Author
+      :docfile: test.adoc
+      :novalid:
+
+      == Clause
+    INPUT
+
+    output = xmlpp(<<~"OUTPUT")
+          #{BLANK_HDR}
+          <preface>#{SECURITY}</preface>
+      <sections>
+      <clause id='_' obligation='normative'>
+             <title>Clause</title>
+             </clause>
+             </sections>
+      </ogc-standard>
+    OUTPUT
+
+    FileUtils.rm_f "test.html"
+    FileUtils.rm_f "test.doc"
+    FileUtils.rm_f "test.pdf"
+    expect(xmlpp(strip_guid(Asciidoctor.convert(input, *OPTIONS))))
+      .to be_equivalent_to output
+    expect(File.exist?("test.html")).to be true
+    expect(File.exist?("test.doc")).to be true
+    expect(File.exist?("test.pdf")).to be true
+  end
+end
