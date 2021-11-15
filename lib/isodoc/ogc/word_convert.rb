88f600c76f4cd74ceefee4e8b2fc93b5a1068cf8
Nick Nicholas <opoudjis@gmail.com>
2021-11-16 01:13:17 +1100

    Word ToC of recommendations, segregate classes: https://github.com/metanorma/metanorma-ogc/issues/293

--- lib/isodoc/ogc/word_convert.rb
+++ lib/isodoc/ogc/word_convert.rb
@@ -86,7 +86,7 @@ module IsoDoc
                           <span lang="EN-GB"><span
         style='mso-element:field-begin'></span><span
         style='mso-spacerun:yes'>&#xA0;</span>TOC
-        \\h \\z \\t &quot;RecommendationTitle,recommendationtitle&quot;#{' '}
+        \\h \\z \\t &quot;RecommendationTitle,RecommendationTestTitle,recommendationtitle,recommendationtesttitle&quot;#{' '}
         <span style='mso-element:field-separator'></span></span>
       TOC
 
@@ -131,11 +131,32 @@ module IsoDoc
 
       def make_RecommendationWordToC(docxml)
         toc = ""
-        docxml.xpath("//p[@class = 'RecommendationTitle']").each do |h|
+        docxml.xpath("//p[@class = 'RecommendationTitle' or @class = 'RecommendationTestTitle']").sort_by do |h|
+          recommmendation_sort_key(h.text)
+        end.each do |h|
           toc += word_toc_entry(1, header_strip(h))
         end
         toc.sub(/(<p class="MsoToc1">)/,
                 %{\\1#{WORD_TOC_RECOMMENDATION_PREFACE1}}) + WORD_TOC_SUFFIX1
+      end
+
+      def recommmendation_sort_key(header)
+        m = /^([^0-9]+) (\d+)/.match(header)
+        "#{recommmendation_sort_key1(m[1])}::#{'%04d' % m[2].to_i}"
+      end
+
+      def recommmendation_sort_key1(type)
+        case type.downcase
+        when "requirement class" then 1
+        when "requirement" then 2
+        when "recommendation" then 3
+        when "permission" then 4
+        when "conformance class" then 5
+        when "abstract test" then 6
+        when "conformance test", "requirement test", "recommendation test",
+         "permission test" then 7
+        else 8
+        end
       end
 
       def make_body2(body, docxml)
@@ -211,8 +232,7 @@ module IsoDoc
         @wordstylesheet = wordstylesheet_update
         Html2Doc.process(
           result,
-          filename: filename,
-          imagedir: @localdir,
+          filename: filename, imagedir: @localdir,
           stylesheet: @wordstylesheet&.path,
           header_file: header&.path, dir: dir,
           asciimathdelims: [@openmathdelim, @closemathdelim],
