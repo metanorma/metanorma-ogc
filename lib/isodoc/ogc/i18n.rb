31a61ea82dc2681e07118db06f3c28cb52cf48cd
Nick Nicholas <opoudjis@gmail.com>
2022-05-31 20:28:49 +1000

    list captions: https://github.com/metanorma/metanorma-iso/issues/663

--- lib/isodoc/ogc/i18n.rb
+++ lib/isodoc/ogc/i18n.rb
@@ -3,7 +3,7 @@ module IsoDoc
     class I18n < IsoDoc::I18n
       def load_yaml1(lang, script)
         y = YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
-        super.merge(y)
+        super.deep_merge(y)
       end
     end
   end
