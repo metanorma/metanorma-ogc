ff5a344096e7347a69d0b079ff23dc375c9692dc
Nick Nicholas <opoudjis@gmail.com>
2023-04-02 00:49:28 +1100

    preprocess XSLT to add colon to notes titles: https://github.com/metanorma/metanorma-ogc/issues/522

--- spec/isodoc/html_convert_spec.rb
+++ spec/isodoc/html_convert_spec.rb
@@ -1726,22 +1726,13 @@ RSpec.describe IsoDoc::Ogc do
                      <bibdata/>
                <metanorma-extension>
                  <render>
-                 <preprocess-xslt format="html,doc">
+                 <preprocess-xslt>
         <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:mn="https://www.metanorma.org/ns/ogc" version="1.0">
           <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="no"/>
           <xsl:preserve-space elements="*"/>
           <xsl:template match="@* | node()">
             <xsl:copy><xsl:apply-templates select="@* | node()"/></xsl:copy>
           </xsl:template>
-          <xsl:template match="mn:note/mn:name">
-            <xsl:copy><xsl:apply-templates select="@*|node()"/><xsl:if test="normalize-space() != ''">:<mn:tab/></xsl:if></xsl:copy>
-          </xsl:template>
-        </xsl:stylesheet>
-      </preprocess-xslt>
-      <preprocess-xslt format="pdf">
-        <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:mn="https://www.metanorma.org/ns/ogc" version="1.0">
-          <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="no"/>
-          <xsl:preserve-space elements="*"/>
           <xsl:template match="mn:note/mn:name">
             <xsl:copy><xsl:apply-templates select="@*|node()"/><xsl:if test="normalize-space() != ''">:<mn:tab/></xsl:if></xsl:copy>
           </xsl:template>
